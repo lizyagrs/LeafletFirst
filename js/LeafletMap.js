1501f5ce3154cff7343b07a9c6f9225b5108e81d
lizyhbu <77211356@qq.com>
2020-02-29 10:08:59 +0800

    Update LeafletMap.js
    修改访问在线地图的版权引用

--- js/LeafletMap.js
+++ js/LeafletMap.js
@@ -1,10 +1,12 @@
 var map;
 function init(){
 	map = L.map("map").setView([30.56486,114.353622 ], 10);  
-	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar'}).addTo(map);
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	foo: 'bar'}).addTo(map);
 	
 	//服务地址
-	var url='http://47.92.246.52:8080/geoserver/LightWebGIS/wms' 
+	var url='localhost:8080/geoserver/LightWebGIS/wms' 
 	//构建图层属性
 	const bounderLayer = L.tileLayer.wms(url, {
 	//图层名称
